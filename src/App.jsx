a909aeab1c5ed95127f19c3697fa1be1bfba3765
Graeme Crawford <graeme_crawford@hotmail.com>
2023-03-31 14:55:34 +1300

    Add PokemonRow component and define props

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,70 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import PropTypes, { arrayOf } from 'prop-types'
 import './App.css'
+import pokemon from './pokemon.json'
+
+const PokemonRow = ({pokemon}) => (
+  <tr>
+    <td>{pokemon.name.english}</td>
+    <td>{pokemon.type.join(", ")}</td>
+  </tr>
+);
+
+PokemonRow.PropTypes = {
+  pokemon: PropTypes.shape({
+    name: PropTypes.shape({
+      english: PropTypes.string,
+    }),
+    type: PropTypes.arrayOf(PropTypes.string),
+  }),
+};
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [filter, setFilter] = React.useState("");
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div
+      style={{
+        margin: "auto",
+        width: 800 ,
+        paddingTop: "1rem",
+      }}
+      >
+      <h1 className='title'>Pokemon Search</h1>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "70% 30%",
+          columnGap: "1rem",
+        }}
+      >
+        <div>
+          <input
+            value={filter}
+            onChange={(evt) => setFilter(evt.target.value)}
+          />
+          <table width="100%">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Type</th>
+              </tr>
+            </thead>
+            <tbody>
+              {pokemon
+                .filter((pokemon) => 
+                  pokemon.name.english.toLowerCase().includes(filter.toLowerCase())
+                )
+                .slice(0,20)
+                .map(pokemon => (
+                  <PokemonRow pokemon={pokemon} key={pokemon.id}/>
+                ))
+              }
+            </tbody>
+          </table>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
