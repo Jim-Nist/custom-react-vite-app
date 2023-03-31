31b9fecce8f6d63b1ae1fb8c4adb26896060feba
Jim-Nist <graeme_crawford@hotmail.com>
2023-03-31 16:21:06 +1300

    Dev to Main (#1)
    * add pokemon.json
    
    * Add PokemonRow component and define props
    
    * add filter function
    
    * conditionally render selected pokemon
    
    * add onSelect function
    
    * Add PokemonInfo Component
    
    * wrap table tr in tbody

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,116 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import propTypes, { arrayOf } from 'prop-types'
 import './App.css'
+import pokemon from './pokemon.json'
+
+const PokemonRow = ({pokemon, onSelect}) => (
+  <tr>
+    <td>{pokemon.name.english}</td>
+    <td>{pokemon.type.join(", ")}</td>
+    <td>
+      <button
+        onClick={() => onSelect(pokemon)}
+      >
+        More Information
+      </button>
+    </td>
+  </tr>
+);
+
+PokemonRow.propTypes = {
+  pokemon: propTypes.shape({
+    name: propTypes.shape({
+      english: propTypes.string,
+    }),
+    type: propTypes.arrayOf(propTypes.string),
+    onSelect: propTypes.func,
+  }),
+};
+
+const PokemonInfo = ({name, base}) => (
+  <div>
+    <h1>{name.english}</h1>
+    <table>
+      <tbody>
+      {
+        Object.keys(base).map((key) => (
+          <tr key={key}>
+            <td>{key}</td>
+            <td>{base[key]}</td>
+          </tr>
+        ))
+      }
+      </tbody>
+    </table>
+  </div>
+);
+
+PokemonInfo.propTypes = {
+    name: propTypes.shape({
+      english: propTypes.string,
+    base: propTypes.shape({
+      HP: propTypes.number.isRequired,
+      Attack: propTypes.number.isRequired,
+      Defense: propTypes.number.isRequired,
+      "Sp. Attack": propTypes.number.isRequired,
+      "Sp. Defense": propTypes.number.isRequired,
+      Speed: propTypes.number.isRequired,
+    })
+  }),
+};
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [filter, setFilter] = React.useState("");
+  const [selectedPokemon, setSelectedPokemon] = React.useState(null);
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div
+      style={{
+        margin: "auto",
+        width: 800 ,
+        paddingTop: "1rem",
+      }}
+      >
+      <h1 className='title'>Pokemon Search</h1>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "70% 30%",
+          columnGap: "1rem",
+        }}
+      >
+        <div>
+          <input
+            value={filter}
+            onChange={(evt) => setFilter(evt.target.value)}
+          />
+          <table width="100%">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Type</th>
+              </tr>
+            </thead>
+            <tbody>
+              {pokemon
+                .filter((pokemon) => 
+                  pokemon.name.english.toLowerCase().includes(filter.toLowerCase())
+                )
+                .slice(0,20)
+                .map(pokemon => (
+                  <PokemonRow 
+                    pokemon={pokemon} 
+                    key={pokemon.id} 
+                    onSelect={(pokemon) => setSelectedPokemon(pokemon)}
+                  />
+                ))
+              }
+            </tbody>
+          </table>
+        </div>
+        {selectedPokemon && <PokemonInfo {...selectedPokemon}/>}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
